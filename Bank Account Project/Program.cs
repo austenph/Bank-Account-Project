654c6f7e67b8df545f43832e4f72eefb8c4f325f
Austen Perelman-Hall <austenph@gmail.com>
2017-01-28 15:34:56 -0500

    created my streamwriter files for checking, reserve, and savings.

--- Bank Account Project/Program.cs
+++ Bank Account Project/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Bank_Account_Project
 {
@@ -10,6 +11,25 @@ namespace Bank_Account_Project
     {
         static void Main(string[] args)
         {
+            //Below our my StreamWriter files for each account type:
+            //checkings
+            StreamWriter checkingsummary = new StreamWriter("CheckingSummary.txt");
+            using (checkingsummary)
+            {
+                checkingsummary.WriteLine("Checking Account" + DateTime.Now.ToShortTimeString());
+            }
+            //reserve
+            StreamWriter reservesummary = new StreamWriter("ReserveSummary.txt");
+            using (reservesummary)
+            {
+                reservesummary.WriteLine("Reserve Account" + DateTime.Now.ToShortTimeString());
+            }
+            //savings
+            StreamWriter savingsummary= new StreamWriter("SavingsSummary.txt");
+            using (savingsummary)
+            {
+                savingsummary.WriteLine("Savings Account" + DateTime.Now.ToShortTimeString());
+            }
         }
     }
 }
